54ffce7b7f3cb42d50b836f3cc388a5b2df03525
Michael Soohoo <mbs729@gmail.com>
2015-11-09 01:28:01 -0800

    Better mergesort.

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,62 +1,41 @@
 #include <iostream>
 using std::cout;
 
-int* merge(int a[], int a_length, int b[], int b_length) {
-  int *sorted = new int[a_length + b_length];
-  int i, j, k;
-  i = j = k = 0;
-  while ((i < a_length) && (j < b_length)) {
-    if (a[i] <= b[j]) {
-      sorted[k] = a[i];
-      i++;
+void merge(int arr[], int length, int start, int mid, int end) {
+  int helper[length];
+  int i;
+  for (i = start; i <= end; i++) {
+    helper[i] = arr[i];
+  }
+
+  int curr = start;
+  int left = start;
+  int right = mid + 1;
+
+  while (left <= mid && right <= end) {
+    if (helper[left] <= helper[right]) {
+      arr[curr++] = helper[left++];
     } else {
-      sorted[k] = b[j];
-      j++;
+      arr[curr++] = helper[right++];
     }
-    k++;
   }
-  for (; i < a_length; i++) {
-    sorted[k] = a[i];
-    k++;
+
+  while (left <= mid) {
+    arr[curr++] = helper[left++];
   }
-  for (; j < b_length; j++) {
-    sorted[k] = b[j];
-    k++;
-  }
-  delete[] a;
-  delete[] b;
-  return sorted;
 }
 
-int* mergesort(int arr[], int length) {
-  if (length == 1) {
-    int *new_arr = new int[1];
-    new_arr[0] = arr[0];
-    return new_arr;
+void mergesort(int arr[], int length, int start, int end) {
+  if (start < end) {
+    int mid = (start + end) / 2;
+    mergesort(arr, length, start, mid);
+    mergesort(arr, length, mid + 1, end);
+    merge(arr, length, start, mid, end);
   }
-  int mid = length / 2;
+}
 
-  int left_length = mid;
-  int *left = new int[left_length];
-  int right_length = length - mid;
-  int *right = new int[right_length];
-
-  int i = 0;
-  int j = 0;
-  for (; i < mid; i++) {
-    left[j] = arr[i];
-    j++;
-  }
-  j = 0;
-  for (; i < length; i++) {
-    right[j] = arr[i];
-    j++;
-  }
-  int *sorted = merge(mergesort(left, left_length), left_length,
-                      mergesort(right, right_length), right_length);
-  delete[] left;
-  delete[] right;
-  return sorted;
+void mergesort(int arr[], int length) {
+  mergesort(arr, length, 0, length-1);
 }
 
 int main() {
@@ -64,18 +43,19 @@ int main() {
   int arr1[] = {2, 3, 8, 8, 7, 7, 7, 8, 9, 10};
   int length2 = 9;
   int arr2[] = {2, 3, 5, 5, 6, 7, 9, 8, 9};
+
   int i;
-  int *sorted1 = mergesort(arr1, length1);
+  mergesort(arr1, length1);
   for (i = 0; i < length1; i++) {
-    cout << sorted1[i] << ' ';
+    cout << arr1[i] << ' ';
   }
   cout << '\n';
-  int *sorted2 = mergesort(arr2, length2);
+
+  mergesort(arr2, length2);
   for (i = 0; i < length2; i++) {
-    cout << sorted2[i] << ' ';
+    cout << arr2[i] << ' ';
   }
   cout << '\n';
-  delete[] sorted1;
-  delete[] sorted2;
+
   return 0;
 }
