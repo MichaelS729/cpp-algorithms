a4db31b37bdca4626fb199af5d9b7d9eea6e39a2
Michael Soohoo <mbs729@gmail.com>
2015-11-15 15:09:13 -0800

    Added iterative binary search

--- SearchAndSort/BinarySearch.cpp
+++ SearchAndSort/BinarySearch.cpp
@@ -16,11 +16,28 @@ int binary_search(int arr[], int val, int left, int right) {
   }
 }
 
+int binary_search_iter(int arr[], int val, int left, int right) {
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    if (val < arr[mid]) {
+      right = mid - 1;
+    } else if (val > arr[mid]) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
+
 int main() {
   int arr[] = {1, 3, 5, 7, 9, 11, 15};
   int arr_length = 7;
   cout << "Should return 2: " << binary_search(arr, 5, 0, arr_length) << '\n';
+  cout << "Should return 2: " << binary_search_iter(arr, 5, 0, arr_length) << '\n';
   cout << "Should return -1: " << binary_search(arr, 6, 0, arr_length) << '\n';
+  cout << "Should return -1: " << binary_search_iter(arr, 6, 0, arr_length) << '\n';
   cout << "Should return 6: " << binary_search(arr, 15, 0, arr_length) << '\n';
+  cout << "Should return 6: " << binary_search_iter(arr, 15, 0, arr_length) << '\n';
   return 0;
 }
